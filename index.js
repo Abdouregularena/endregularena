e8e6e4ef0e5d3df6ab1d01c2725b951f0ea7f140
Abdou NDAO <abdou@regularena.com>
2026-05-16 00:06:43 +0000

    Deploy test Railway

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello depuis Railway 🚀");
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "API en ligne ✅" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur le port ${PORT}`);
+});
