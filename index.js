78c084fe76c805e61484e7ce735f5acc5bbc1f99
Abdou NDAO <abdou@regularena.com>
2026-05-19 14:43:02 +0000

    fix line 264

--- index.js
+++ index.js
@@ -261,7 +261,7 @@ app.use(helmet({
   },
 }));
 app.use((req, res, next) => {
-  const allowed = ['https://www.regularena.com','https://regularena.com',''https://endregularena-production.up.railway.app'];
+  const allowed = ['https://www.regularena.com','https://regularena.com','https://endregularena-production.up.railway.app'];
   if (allowed.includes(req.headers.origin)) res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
